c5ecec89821ee575bd425857fda63661bcd81b33
YogyerekDEV <mszebi2008@gmail.com>
2026-01-21 12:18:49 +0100

    added getAll, getById

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 
@@ -9,5 +9,15 @@ export class UsersController {
   @Post('new')
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Get('getAll')
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get('getById/:id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(Number(id));
   }
 }
